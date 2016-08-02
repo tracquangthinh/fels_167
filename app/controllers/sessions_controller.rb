96de65cd2b916c1ef11a1d9388acf55b6cd1fccf
Kieu Hai Dang <kieu.hai.dang@framgia.com>
2016-08-02 10:26:02 +0700

    Login Logout
    2nd login logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by email: user_params[:email]
+    if user && user.authenticate(user_params[:password])
+      login user
+      flash[:success] = t(:login_success)
+      redirect_to help_path
+    else
+      flash[:danger] = t(:login_false)
+      render :new
+    end
+  end
+
+  def destroy
+    logout current_user
+    redirect_to home_path
+  end
+
+  private
+  def user_params
+    params.require(:session).permit :email, :password
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+
   get "/help", to: "static_pages#help"
   get "/about",to: "static_pages#about"
   get "/home", to: "static_pages#home"
-
-root "static_pages#home"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  resources :user
+  root "static_pages#home"
 end
