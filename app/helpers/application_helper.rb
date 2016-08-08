ed1bf6017d664881af35d2e575657598f0115128
Kieu Hai Dang <kieu.hai.dang@framgia.com>
2016-08-08 17:00:59 +0700

    User show categories

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def avatar_for user, options = {size: 80}
+    if user.avatar?
+      image_tag user.avatar, class: "my-avatar-home"
+    else
+      image_tag "medium/missing.png", class: "my-avatar-home"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
     resources :categories
     resources :words
   end
+  resources :categories
 end
